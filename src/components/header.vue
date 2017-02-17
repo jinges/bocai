322c805ec91e07c127500ddfdf2149dcf55d485b
unknown <811357810@qq.com>
2017-02-18 00:21:54 +0800

    'header'

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,16 +1,35 @@
 <template>
 	<article class="header">
-		<ul>
-			<li><span class="portrait"></span>qqqq12345</li>
-			<li class="money">￥0.00</li>
-			<li>游戏大厅</li>
-			<li>优惠活动</li>
-			<li>银行资料</li>
-			<li>资金密码</li>
-			<li>团队统计</li>
-			<li>转账</li>
-			<li><i></i>充值</li>
-			<li>理财</li>
-			<li>安全退出</li>
+		<ul class="login">
+			<li class="bor-r"><span class="portrait"></span>qqqq12345</li>
+			<li class="bor-r bor-l money">￥0.00</li>
+			<li class="bor-r bor-l ">游戏大厅</li>
+			<li class="bor-r bor-l "><i class="activity icon"></i><span class="txt">优惠活动</span></li>
+			<li class="bor-r bor-l ">银行资料</li>
+			<li class="bor-r bor-l ">资金密码</li>
+			<li class="bor-r bor-l ">团队统计</li>
+			<li class="bor-r bor-l ">转账</li>
+			<li class="bor-r bor-l "><i class="pay icon"></i><span class="txt">充值</span></li>
+			<li class="bor-r bor-l "><i class="financing icon"></i> <span class="txt">理财</span></li>
+			<li class="bor-l "style="color: #ff5c5c"><i class="logout icon"></i><span class="txt">安全退出</span></li>
 		</ul>
+		<div class="no_login bw">
+			<div class="btn-area wb"><a class="btn-login"></a></div>
+			<div class="login_form wb">
+				<input class="name" type="text" name="" placeholder="请输入用户名">
+				<input class="password" type="text" name="" placeholder="请输入登录密码">
+				<input class="code" type="text" name="" placeholder="验证码">
+				<span class="code-area"><img :src="codeImg" class="codeimg"><span class="code-font">验证码区</span></span>
+			</div>
+			
+		</div>
 </template>
+<script>
+export default{
+	data(){
+		return{
+			codeImg: "/dist/assets/img/code_img.png",
+		}
+	}
+}
+</script>
--- src/components/nav_header.vue
+++ src/components/nav_header.vue
@@ -10,6 +10,17 @@
 			</ul>
 		</div>
 		<p class="clear prompt">{{navData.tips.title}} <span class="example">范例</span> <span class="help">？</span></p>
+		<div class="text-box">
+			<div class="single">
+				<textarea class="input_nums" style="overflow-y: hidden; outline: none;"></textarea>
+				 <span class="clear-repeat">删除重复</span>
+		        <span class="empty">清空</span>
+				<span class="write_box_desc">
+		            每一个号码之间请用一个 空格[ ]、逗号[,] 或者 分号[;] 隔开
+		        </span>
+		       
+			</div>
+		</div>
 	</article>
 </template>
 <script>
